7197f675dfef253c6896185a3a41b6d881692b21
abelbonillagarcia@gmail.com <abelbonillagarcia@gmail.com>
2025-03-06 22:34:50 -0500

    Proyecto tuteca

---
+++ src/OA.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class OA {
+public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        int numero1 = input.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int numero2 = input.nextInt();
+
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        int division = numero1 / numero2;
+        int modulo = numero1 % numero2;
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
+        System.out.println("Módulo: " + modulo);
+
+        input.close();
+}    
+}
